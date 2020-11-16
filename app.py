35fde23408f5afb1c9ab0f19437a4e8c5c8a1e29
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-11-16 11:22:37 -0700

    first portion finished

--- app.py
+++ app.py
@@ -8,15 +8,5 @@ def home():
     return render_template('index.html', name="jayden")
 
 
-@app.route('/slug/<id>')
-def slug(id):
-    return render_template('slug.html', id=id)
-
-
-@app.route('/loops')
-def loops():
-    return render_template('loops.html', content=['Cory', 'Zac', 'Jayden', 'Boli', 'Eva'])
-
-
 if __name__ == "__main__":
     app.run(debug=True)
